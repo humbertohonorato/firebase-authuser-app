be1b816b8e5d60e448e73f17315d0cd33f0388f2
humbertohonorato <humberto.honorato@outlook.com>
2024-09-25 21:31:47 -0300

    configurando variaveis ambiente

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2S_DbUMkNQfJRocpsvA0jTJzV7OrnYig",
-  authDomain: "fir-authuser-app.firebaseapp.com",
-  projectId: "fir-authuser-app",
-  storageBucket: "fir-authuser-app.appspot.com",
-  messagingSenderId: "863797055193",
-  appId: "1:863797055193:web:0ef614524d9917f0db78c8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
